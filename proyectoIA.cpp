c6ce2388cc4a1a8474ecf43452ffc3985b151481
Sebaatd <sebaatd@gmail.com>
2018-12-09 14:20:15 -0300

    Save

--- proyectoIA.cpp
+++ proyectoIA.cpp
@@ -6,12 +6,12 @@
 using namespace std;
 
 void lecturaInput(int &cant_autos, int &cant_opciones, int &cant_clases, vector<int> &maxCantAutosBloq, 
-	vector<int> &tamBloq, vector<int> &autos_por_clase, vector<vector<int>> &opciones_por_clase){
+	vector<int> &tamBloq, vector<int> &autos_por_clase, vector<vector<int>> &opciones_por_clase, vector<vector<int>> &dominios, vector<int> &solucion){
 
 	string line;
 	int cont = 0;
 	vector<string> result;
-	vector<int> opciones;
+	vector<int> opciones, clases;
 
 	while (getline(cin, line)){
 		//Primera línea: Obtención de cantidades de: autos, opciones y clases.
@@ -26,6 +26,16 @@ void lecturaInput(int &cant_autos, int &cant_opciones, int &cant_clases, vector<
 		    cant_opciones = stoi(result[1]);
 		    cant_clases = stoi(result[2]);
 		    result.clear();
+
+		    for(int i = 0; i < cant_clases; i++){
+		    	clases.push_back(i);
+		    }
+
+	        for(int i = 0; i < cant_autos; i++){
+    			dominios.push_back(clases);
+    			solucion.push_back(-1);
+    		}
+
 		}
 
 		//Segunda línea: Obtención de tamaño máximo de autos por bloques.
@@ -78,15 +88,104 @@ void lecturaInput(int &cant_autos, int &cant_opciones, int &cant_clases, vector<
 
 		cont++;
     }
+	
+}
+
+void ForwardChecking(int nivel, vector<int> solucion, int cant_clases, vector<vector<int>> dominios, vector<int> autos_por_clase){
+	int clase_instanciada;
+	vector<int> clases_disponibles(cant_clases), autos_disponibles_por_clase = autos_por_clase;
+	vector<vector<int>> dominios_filtrados = dominios;
+
+	iota(clases_disponibles.begin(), clases_disponibles.end(), 0);
+
+	if(clases_disponibles.size() != 0){
+		clase_instanciada = clases_disponibles[0];
+		solucion[nivel] = clase_instanciada;
+
+
+		clases_disponibles.erase(clases_disponibles.begin()); //OJO QUE SE BORRA SIEMPRE LA PRIMERA
+		dominios_filtrados.erase(dominios_filtrados.begin()+nivel);
+
+		for(size_t i = 0; i < dominios_filtrados.size(); i++){
+			//ELIMINAR TODOS LOS VALORES INCONSISTENTES
+
+		}
+
+
+	}
+}
+
+void FC(int nivel, int cant_clases, vector<int> autos_por_clase, vector<int> &solucion, vector<vector<int>> dominios){
+	
+	//Seleccionar X_i
+	for(int i = 0; i < cant_clases; i++){
+
+		//Instanciar X_i
+		if(autos_por_clase[i] != 0){
+			solucion[nivel] = i;
+			autos_por_clase[i]--;
+			//dominios.erase(dominios.begin()+i);
+
+			//Razonar hacia adelante (forward-check)
+			for(size_t j = 0; j < dominios.size(); j++){
+
+				for(size_t k = 0; k < dominios[j].size(); k++){
+					if(dominios[j][k] == i && autos_por_clase[i] == 0){
+						dominios[j][k] = -1;
+					}
+					cout << dominios[j][k];
+				}
+				cout << endl;
+				
+			}
+			cout << endl;
+		}
+
+		break;
+	}
+
 
 }
 
-int main(){
-	int cant_autos, cant_opciones, cant_clases;
-	vector<int> maxCantAutosBloq, tamBloq, autos_por_clase;
-	vector<vector<int>> opciones_por_clase;
+int tresFC(int nivel, vector<int> &solucion){
+	if(nivel == 9){
+		return 1;
+	}
+	
 
-	lecturaInput(cant_autos, cant_opciones, cant_clases, maxCantAutosBloq, tamBloq, autos_por_clase, opciones_por_clase);
+}
+
+
+vector<int> tercerFC(vector<int> solucion){
+	vector<int> secuencia_final = solucion;
+	int i = tresFC(0, secuencia_final);
+	return secuencia_final;
+}
+
+
+int main(){
+	/*
+	Variables:
+		-cant_autos: Valor del total de autos.
+		-cant_opciones: Valor del total de opciones.
+		-cant_clases: Valor del total de clases distintas.
+
+	Vectores:
+		-maxCantAutosBloq: Vector que almacena los máximos de autos por bloque, donde el índice es el número de la opción.
+		-tamBloq: Vector que almacena el tamaño del bloque, donde el índice es el número de la opción.
+		-autos_por_clase: Vector que almacena la cantidad de autos de cada clase, donde el índice es el número de la clase.
+		-opciones_por_clase: Vector de 2 dimensiones que almacena las opciones de cada clase, donde el índice es el número de la clase.
+	*/
+
+	int cant_autos, cant_opciones, cant_clases;
+	vector<int> maxCantAutosBloq, tamBloq, autos_por_clase, solucion;
+	vector<vector<int>> opciones_por_clase, dominios;
+
+	lecturaInput(cant_autos, cant_opciones, cant_clases, maxCantAutosBloq, tamBloq, autos_por_clase, opciones_por_clase, dominios, solucion);
+
+	//ForwardChecking(0, solucion, cant_clases, dominios, autos_por_clase);
+	FC(0, cant_clases, autos_por_clase, solucion, dominios);
+
 
 	/* REVISION INPUT
 	cout << cant_autos << " " << cant_opciones << " " << cant_clases << endl;
@@ -109,5 +208,16 @@ int main(){
 	}
 	cout << endl;
 	*/
+
+	/*
+	for(int i = 0; i < cant_autos; i++){
+		cout << "Posición " << i << ": ";
+		for(int j = 0; j < cant_clases; j++){
+			cout << dominios[i][j] << " ";
+		}
+		cout << endl;
+	}
+	*/
+	
 
 }
